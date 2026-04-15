1b0559cd586056532771c28be5479daf3b2360d7
sabiha070 <sabihashaik070@gmail.com>
2026-04-15 08:25:52 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,21 +1,52 @@
-from django.shortcuts import render
-from users.forms import DriverRegistrationForm
+from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from users.models import DriverRegistrationModel,FattigueInfoModel
 
 
-def index(request):
-    return render(request, 'index.html', {})
+# Create your views here.
 
-
-def logout(request):
-    return render(request, 'index.html', {})
-
-def AutoistLogin(request):
-    return render(request, 'AutoistLogin.html', {})
-
-def AdminLogin(request):
+def AdminLoginCheck(request):
+    if request.method == 'POST':
+        usrid = request.POST.get('loginid')
+        pswd = request.POST.get('pswd')
+        print("User ID is = ", usrid)
+        if usrid == 'admin' and pswd == 'admin':
+            return render(request, 'admins/AdminHome.html')
+        elif usrid == 'Admin' and pswd == 'Admin':
+            return render(request, 'admins/AdminHome.html')
+        else:
+            messages.success(request, 'Please Check Your Login Details')
     return render(request, 'AdminLogin.html', {})
 
 
-def AutoistRegister(request):
-    form = DriverRegistrationForm()
-    return render(request, 'AutoistRegister.html', {'form': form})
+def AdminHome(request):
+    return render(request, 'admins/AdminHome.html')
+
+
+def ViewRegisteredAutoists(request):
+    data = DriverRegistrationModel.objects.all()
+    return render(request, 'admins/ViewRegisterAutoists.html', {'data': data})
+
+
+def AdminActivaAutoists(request):
+    if request.method == 'GET':
+        id = request.GET.get('uid')
+        status = 'activated'
+        print("PID = ", id, status)
+        DriverRegistrationModel.objects.filter(id=id).update(status=status)
+        data = DriverRegistrationModel.objects.all()
+        return render(request, 'admins/ViewRegisterAutoists.html', {'data': data})
+
+
+def AdminDeleteAutoists(request):
+    if request.method == 'GET':
+        id = request.GET.get('uid')
+        DriverRegistrationModel.objects.filter(id=id).delete()
+        data = DriverRegistrationModel.objects.all()
+        return render(request, 'admins/ViewRegisterAutoists.html', {'data': data})
+
+
+def AdminViewFatigueHistory(request):
+    qs = FattigueInfoModel.objects.all()
+    return render(request, 'admins/ViewAutoistsHistory.html', {'data': qs})
+
